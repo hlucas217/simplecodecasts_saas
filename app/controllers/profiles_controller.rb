e75e237d86ceb6b92a0f19a7a9d5fbcac7043673
Heather Lucas <hlucas@gmail.com>
2016-07-27 02:27:44 +0000

    Added users show action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,7 +4,6 @@ class ProfilesController < ApplicationController
        @user = User.find( params[:user_id] )
        @profile = @user.build_profile
    end
-   
    def create
        @user = User.find( params[:user_id] )
        @profile = @user.build_profile(profile_params)
@@ -15,9 +14,8 @@ class ProfilesController < ApplicationController
            render action: :new
        end
    end
-   
    private
-   def profile_params
-       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
-   end
+    def profile_params
+        params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    end
 end
